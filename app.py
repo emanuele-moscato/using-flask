fa55bfc26d3f10a8eb955127f1db3292c63772b3
Emanuele <emanuele@asiadmins-MacBook-Pro-2.local>
2018-10-05 15:23:54 +0100

    Add logging and template rendering to the Flask app, add a basic html template

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask, make_response, request, jsonify
+from flask import Flask, make_response, request, jsonify, url_for, \
+    redirect, abort, render_template
+import logging
+import daiquiri
+import sys
+import os
 
 PORT = 8000
+LOGS_DIR = './logs/'
+
+if not os.path.isdir(LOGS_DIR):
+    os.mkdir(LOGS_DIR)
+
+daiquiri.setup(
+    level=logging.INFO,
+    outputs=(
+        daiquiri.output.Stream(sys.stdout),
+        daiquiri.output.File(
+            os.path.join(LOGS_DIR, 'api_logs.log'),
+            formatter=daiquiri.formatter.TEXT_FORMATTER
+        )
+    )
+)
+
+logger = daiquiri.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -20,12 +42,12 @@ e della vita il doloroso amore."""
 
 endpoints = ['/', '/ulisse/', '/instructions/', '/greet-user/<username>/']
 
-instructions = """Endpoints: {}""".format("".join(endpoints))
+instructions = "Hit other endpoints."
 
 
 @app.route('/')
 def say_something():
-    return "ACTIVE (hit /instructions)"
+    return redirect(url_for('say_instructions'))
 
 
 @app.route('/ulisse/')
@@ -38,23 +60,46 @@ def say_instructions():
     return instructions
 
 
+@app.route("/abort/")
+def abort_endpoint():
+    abort(401)
+
+
+@app.route('/greet-user/')
 @app.route('/greet-user/<string:username>/')
-def greet_user(username):
-    return "Hi "+username
+def greet_user(username=None):
+    if username:
+        return "Hi "+username
+    else:
+        return "Hi"
 
 
 @app.route('/greet-person/', methods=['GET', 'POST'])
 def greet_person():
     if request.method == 'GET':
-        response_body = {'response': 'Send a POST request'}
+        response_body = {
+            'url': url_for('greet_person'),
+            'response': 'Send a POST request'
+        }
         return make_response(jsonify(response_body), 200)
     elif request.method == 'POST':
         r = request.json
-        response_body = {'response': "Hi, "+r['person']}
+        response_body = {
+            'url': url_for('greet_person'),
+            'response': "Hi, "+r['person']
+        }
         return make_response(jsonify(response_body), 200)
     else:
         pass
 
 
+@app.route('/index/')
+def render_index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
-    app.run(port=PORT, debug=True)
+    try:
+        app.run(port=PORT, debug=True)
+    except Exception:
+        logger.error("Error executing main routine", exc_info=True)
