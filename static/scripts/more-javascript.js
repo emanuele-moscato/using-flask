568f82e66774664553156d5fb03e85c3aa48f318
Emanuele <emanuele@asiadmins-MacBook-Pro-2.local>
2018-10-10 16:07:50 +0100

    Update the more-javascript section

--- static/scripts/more-javascript.js
+++ static/scripts/more-javascript.js
@@ -6,3 +6,24 @@ function changePar() {
     document.getElementById("changeable").innerHTML =
       "Paragraph changed to "+z+".";
 }
+function populateDiv() {
+  parToShow = "<h2>Populated!</h2><p>Ahi ch'a dir qual era è cosa dura</p>"
+  document.getElementById("d-container").innerHTML = parToShow
+}
+var benji = {
+  name: "Benji",
+  iq: 12,
+  height: 175,
+  weight: 999,
+  sayHi: function() {
+    return "Hi, I am"+this.name+"!";
+  }
+}
+function printBenji() {
+  document.getElementById("benjipar").innerHTML =
+    benji.sayHi()+
+    "<br>"+
+    "Benji is "+benji.iq+", is "+benji.height+" cm tall and weighs "+
+    benji.weight+" kg.";
+  document.getElementById("benji-button").style.display = "none";
+}
